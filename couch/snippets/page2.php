1aacfc3519d07c8fdc5c2147eaf3b6243a9266ef
sandraonne <sandra.onne@khk.ee>
2018-03-10 15:00:55 +0200

    Add calendar

--- couch/snippets/page2.php
+++ couch/snippets/page2.php
@@ -1,4 +1,50 @@
+   <section class="mbr-section content4 cid-qLBazTmo1t grid-12" id="content4-16">  
+        <div class="container col-md-12" align-right>
+            <div class="media-container-row">
+                <!-- List view -->
+                <cms:calendar date="<cms:gpc 'cal' />" week_starts='1' masterpage='events.php' show_future_entries='1'>
+                    <table class="calendar_small">
+                        <tr>
+                           <th><a href="<cms:concat k_page_link '?cal=' k_prev_calendar_date />"> << </a></th>
+                            <th colspan="5"><cms:date k_calendar_date format='%B %Y' locale="estonian" charset="ISO-8859-4" /></th>
+                            <th><a href="<cms:concat k_page_link '?cal=' k_next_calendar_date />"> >> </a></th>
+                        </tr>
+                        <tr>
+                            <cms:repeat count='7'>
+                            <td class="months_heading"><cms:zebra 'E' 'T' 'K' 'N' 'R' 'L' 'P'/></td>
+                            </cms:repeat>
+                        </tr>
 
+                        <cms:weeks>
+                            <tr>
+                            <cms:days >
+                                <cms:if k_timeline_position='present'>
+                                    <cms:set tdclass='today' />
+                                <cms:else />
+                                    <cms:set tdclass='' />
+                                </cms:if>
+
+                                <cms:if k_position='current_month' >
+                                    <cms:if k_count_entries >
+                                        <td class='entries <cms:show tdclass />' >
+                                            <a href="<cms:link masterpage='events.php' year=k_year month=k_month day=k_day />"><cms:show k_day /></a>
+                                        </td>
+                                    <cms:else />
+                                        <td class='<cms:show tdclass />' ><cms:show k_day /></td>
+                                    </cms:if>
+                                <cms:else />
+                                    <td class='other_month'><cms:show k_day /></td>
+                                </cms:if>
+                            </cms:days>
+                            </tr>
+                        </cms:weeks>
+                    </table>
+                </cms:calendar>
+            </div>
+        </div>
+    </section>
+
+   
     <section class="mbr-section content4 cid-qLBazTmo1t" id="content4-16">  
 
         <div class="container">
--- header.php
+++ header.php
@@ -19,6 +19,7 @@
         <link rel="stylesheet" href="assets/theme/css/style.css">
         <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
         <link rel="stylesheet" href="assets/flag-icon-css/css/flag-icon.css">
+        <link rel="stylesheet" href="calendar.css"
 
 
     </head>
