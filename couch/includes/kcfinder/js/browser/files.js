3303a02282f59c601b6eb28093c19b094246e817
Andres Soop <andressoop@gmail.com>
2018-03-12 16:55:35 +0200

    Fix CoachCMS php error
    Removed php tags from JavaScript files

--- couch/includes/kcfinder/js/browser/files.js
+++ couch/includes/kcfinder/js/browser/files.js
@@ -1,4 +1,3 @@
-<?php
 
 /** This file is part of KCFinder project
   *
@@ -10,7 +9,7 @@
   *   @license http://www.opensource.org/licenses/gpl-2.0.php GPLv2
   *   @license http://www.opensource.org/licenses/lgpl-2.1.php LGPLv2
   *      @link http://kcfinder.sunhater.com
-  */?>
+  */
 
 browser.initFiles = function() {
     $(document).unbind('keydown');
