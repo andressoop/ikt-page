fd45f4e4696990a9f75d2ceffd39bd975c5441a2
Andres Soop <andressoop@gmail.com>
2018-09-21 23:18:23 +0300

    Refactor 'Õppetöö' and 'Praktika' pages
    Make the code much more lean and remove the conflict on loading the page admin section

--- oppetoo.php
+++ oppetoo.php
@@ -3,46 +3,37 @@
 
     <cms:template title='Õppetöö' />
 
-    <cms:editable name='main_studies' label="Õppematerjalid" type='richtext'>
     <section class="mbr-section content4 cid-qLBfffq1Ei">
-
         <div class="container">
             <div class="media-container-row">
                 <div class="title col-12 col-md-8">
-
-                    <h2 class="align-center pb-3 mbr-fonts-style display-2">Õppematerjalid</h2>
-
+                    <h2 class="align-center pb-3 mbr-fonts-style display-2">
+                        <cms:editable name='oppetoo_title_first' label="Lõigu pealkiri" type='text'>Õppematerjalid</cms:editable>
+                    </h2>
                 </div>
             </div>
         </div>
     </section>
 
     <section class="mbr-section article content12 eriala-content">
-
         <div class="container">
             <div class="media-container-row">
                 <div class="mbr-text counter-container col-12 col-md-8 mbr-fonts-style display-7">
+                    <cms:editable name='oppetoo_body_first' label="Lõigu tekst" type='richtext'>
 
-                    <div><strong>Noorem tarkvaraarendaja (veebispetsialist)</strong></div><div><strong><br></strong></div><ul>
-                        <li><strong>Materjal 1&nbsp;</strong> - Materjali lühikirjeldus. <a href="http://at13.ikt.khk.ee">Vaata siit!</a></li>
-                        <li><strong>Materjal 2&nbsp;</strong>- Materjali lühikirjeldus. <a href="http://at13.ikt.khk.ee">Vaata siit!</a></li>
-                        <li><strong>Materjal 3</strong> - Materjali lühikirjeldus. <a href="http://at13.ikt.khk.ee">Vaata siit!</a></li>
-                    </ul>
+                    </cms:editable>
                 </div>
             </div>
         </div>
     </section>
-    </cms:editable>
 
-    <cms:editable name='main_konsultatsioon' label="Konsultatsiooniajad" type='richtext'>
     <section class="mbr-section article content12 cid-qLBffhcnrA">
-
            <div class="container">
             <div class="media-container-row">
                 <div class="title col-12 col-md-8">
-
-                    <h2 class="align-center pb-3 mbr-fonts-style display-2">Konsultatsiooniajad</h2>
-
+                    <h2 class="align-center pb-3 mbr-fonts-style display-2">
+                        <cms:editable name='oppetoo_title_second' label="Lõigu pealkiri" type='text'>Konsultatsiooniajad</cms:editable>
+                    </h2>
                 </div>
             </div>
         </div>
@@ -50,14 +41,13 @@
         <div class="container">
             <div class="media-container-row">
                 <div class="mbr-text counter-container col-12 col-md-8 mbr-fonts-style display-7">
-                    <div><strong>Õpetaja 1</strong></div><div><strong><br></strong></div><ul>
-                        <li><strong>Aeg 1</strong> - lühikirjeldus. </li>
-                    </ul>
+                    <cms:editable name='oppetoo_body_second' label="Lõigu tekst" type='richtext'>
+
+                    </cms:editable>
                 </div>
             </div>
         </div>
     </section>
-    </cms:editable>
 
     <cms:embed "footer.php" />
 <?php COUCH::invoke(); ?>
--- praktika.php
+++ praktika.php
@@ -3,54 +3,27 @@
 
     <cms:template title='Praktika' />
 
-    <cms:editable name='main_content' label="Praktikapakkumised" type='richtext'>
     <section class="mbr-section content4 cid-qLBdf21SG5">
-
-
-
         <div class="container">
             <div class="media-container-row">
                 <div class="title col-12 col-md-8">
                     <h2 class="align-center pb-3 mbr-fonts-style display-2">Praktikapakkumised</h2>
-
-
                 </div>
             </div>
         </div>
     </section>
 
     <section class="mbr-section article content12 cid-qLBdC3oald">
-
-
         <div class="container">
             <div class="media-container-row">
                 <div class="mbr-text counter-container col-12 col-md-8 mbr-fonts-style display-7">
-                    <div><strong>Noorem tarkvaraarendaja (veebispetsialist)</strong></div><div><strong><br></strong></div><ul>
-                        <li><strong>Ettevõte&nbsp;</strong> - Pakkumise lühikirjeldus. <a href="http://at13.ikt.khk.ee">Vaata siit!</a></li>
-                        <li><strong>Ettevõte&nbsp;</strong>- Pakkumise lühikirjeldus. <a href="http://at13.ikt.khk.ee">Vaata siit!</a></li>
-                        <li><strong>Ettevõte</strong>&nbsp;- Pakkumise lühikirjeldus. <a href="http://at13.ikt.khk.ee">Vaata siit!</a></li>
-                    </ul>
+                    <cms:editable name='main_content' label="Praktikapakkumised" type='richtext'>
+                        
+                    </cms:editable>
                 </div>
             </div>
         </div>
     </section>
-
-    <section class="mbr-section article content12 cid-qLBeYTfcvq">
-
-
-        <div class="container">
-            <div class="media-container-row">
-                <div class="mbr-text counter-container col-12 col-md-8 mbr-fonts-style display-7">
-                    <div><strong>Bürootöö</strong></div><div><strong><br></strong></div><ul>
-                        <li><strong>Ettevõte&nbsp;</strong> - Pakkumise lühikirjeldus. <a href="http://at13.ikt.khk.ee">Vaata siit!</a></li>
-                        <li><strong>Ettevõte&nbsp;</strong>- Pakkumise lühikirjeldus. <a href="http://at13.ikt.khk.ee">Vaata siit!</a></li>
-                        <li><strong>Ettevõte</strong>&nbsp;- Pakkumise lühikirjeldus. <a href="http://at13.ikt.khk.ee">Vaata siit!</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </section>
-    </cms:editable>
 
     <cms:embed "footer.php" />
 <?php COUCH::invoke(); ?>
