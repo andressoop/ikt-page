df6f959b9ecb2a94e7e560f7945af73c69c6c071
Andres Soop <andressoop@gmail.com>
2018-09-29 14:50:56 +0300

    Add internship and study material translations

--- oppetoo.php
+++ oppetoo.php
@@ -1,14 +1,23 @@
 <?php require_once( 'couch/cms.php' ); ?>
     <cms:embed "header.php" />
+    <cms:template title='Õppetöö' order="7">
+        <cms:editable name='oppetoo1_title_ee' label="(EST) Lõigu pealkiri" order="1" type='text' />
+        <cms:editable name='oppetoo1_title_en' label="(ENG) Lõigu pealkiri" order="2" type='text' />
+        <cms:editable name='oppetoo1_content_ee' label="(EST) Lõigu tekst" order="3" type='richtext' />
+        <cms:editable name='oppetoo1_content_en' label="(ENG) Lõigu tekst" order="4" type='richtext' />
 
-    <cms:template title='Õppetöö' order="7" />
+        <cms:editable name='oppetoo2_title_ee' label="(EST) Lõigu pealkiri" order="5" type='text' />
+        <cms:editable name='oppetoo2_title_en' label="(ENG) Lõigu pealkiri" order="6" type='text' />
+        <cms:editable name='oppetoo2_content_ee' label="(EST) Lõigu tekst" order="7" type='richtext' />
+        <cms:editable name='oppetoo2_content_en' label="(ENG) Lõigu tekst" order="8" type='richtext' />
+    </cms:template>
 
     <section class="oppetoo-title">
         <div class="container">
             <div class="media-container-row">
                 <div class="title col-12 col-md-8">
                     <h2 class="align-center pb-3 mbr-fonts-style display-2">
-                        <cms:editable name='oppetoo_title_first' label="Lõigu pealkiri" type='text'>Õppematerjalid</cms:editable>
+                        <cms:get "oppetoo1_title_<cms:show k_lang />" />
                     </h2>
                 </div>
             </div>
@@ -19,9 +28,7 @@
         <div class="container">
             <div class="media-container-row">
                 <div class="mbr-text counter-container col-12 col-md-8 mbr-fonts-style display-7">
-                    <cms:editable name='oppetoo_body_first' label="Lõigu tekst" type='richtext'>
-
-                    </cms:editable>
+                    <cms:get "oppetoo1_content_<cms:show k_lang />" />
                 </div>
             </div>
         </div>
@@ -32,7 +39,7 @@
             <div class="media-container-row">
                 <div class="title col-12 col-md-8">
                     <h2 class="align-center pb-3 mbr-fonts-style display-2">
-                        <cms:editable name='oppetoo_title_second' label="Lõigu pealkiri" type='text'>Konsultatsiooniajad</cms:editable>
+                        <cms:get "oppetoo2_title_<cms:show k_lang />" />
                     </h2>
                 </div>
             </div>
@@ -43,9 +50,7 @@
         <div class="container">
             <div class="media-container-row">
                 <div class="mbr-text counter-container col-12 col-md-8 mbr-fonts-style display-7">
-                    <cms:editable name='oppetoo_body_second' label="Lõigu tekst" type='richtext'>
-
-                    </cms:editable>
+                    <cms:get "oppetoo2_content_<cms:show k_lang />" />
                 </div>
             </div>
         </div>
--- praktika.php
+++ praktika.php
@@ -1,13 +1,19 @@
 <?php require_once( 'couch/cms.php' ); ?>
     <cms:embed "header.php" />
-
-    <cms:template title='Praktika' order="6" />
+    <cms:template title='Praktika' order="6">
+        <cms:editable name='praktika_title_ee' label="(EST) Lõigu pealkiri" order="1" type='text' />
+        <cms:editable name='praktika_title_en' label="(ENG) Lõigu pealkiri" order="2" type='text' />
+        <cms:editable name='praktika_content_ee' label="(EST) Lõigu tekst" order="3" type='richtext' />
+        <cms:editable name='praktika_content_en' label="(ENG) Lõigu tekst" order="4" type='richtext' />
+    </cms:template>
 
     <section class="praktika-title">
         <div class="container">
             <div class="media-container-row">
                 <div class="title col-12 col-md-8">
-                    <h2 class="align-center pb-3 mbr-fonts-style display-2">Praktikapakkumised</h2>
+                    <h2 class="align-center pb-3 mbr-fonts-style display-2">
+                        <cms:get "praktika_title_<cms:show k_lang />" />
+                    </h2>
                 </div>
             </div>
         </div>
@@ -17,9 +23,7 @@
         <div class="container">
             <div class="media-container-row">
                 <div class="mbr-text counter-container col-12 col-md-8 mbr-fonts-style display-7">
-                    <cms:editable name='main_content' label="Praktikapakkumised" type='richtext'>
-
-                    </cms:editable>
+                    <cms:get "praktika_content_<cms:show k_lang />" />
                 </div>
             </div>
         </div>
