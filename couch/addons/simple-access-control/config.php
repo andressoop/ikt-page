82da01bb30460e4e637404c34592bfdb70985ca1
Andres Soop <andressoop@gmail.com>
2018-12-06 13:25:11 +0200

    Add permissions for superadmin

--- couch/addons/simple-access-control/config.php
+++ couch/addons/simple-access-control/config.php
@@ -11,3 +11,24 @@
     // $t['blog.php'] = 'admin';   // only user 'admin' has access
     // $t['news.php'] = 'admin | jane | joe';     // users 'admin', 'jane', 'jone' have access
     // $t['test.php'] = '';      // no one has access!!
+
+
+    // Avaleht
+    $t['index.php'] = 'osakondiktkhk';
+    // Osakonna tutvustus
+    $t['osakond.php'] = 'osakondiktkhk';
+    // Erialad
+    $t['erialad.php'] = 'osakondiktkhk';
+    // Uudised
+    $t['uudised.php'] = 'osakondiktkhk';
+    $t['postitused.php'] = 'osakondiktkhk';
+    // Kalender
+    $t['kalender.php'] = 'osakondiktkhk';
+    // Praktika
+    $t['praktika.php'] = 'osakondiktkhk';
+    // Õppetöö
+    $t['oppetoo.php'] = 'osakondiktkhk';
+    // Üldised sätted, footer
+    $t['globals.php'] = 'osakondiktkhk';
+    // Tõlked
+    $t['translations.php'] = 'osakondiktkhk';
